b3e0aea76dc3eed417805a8224abaaf6beefc9f8
emma <emma.abe.bradford@gmail.com>
2022-03-14 13:02:08 -0400

    updated app controller

--- backend/seniordesign/src/app.module.ts
+++ backend/seniordesign/src/app.module.ts
@@ -7,12 +7,15 @@ import { BooksController } from './Book/book.controller';
 import { BooksModule } from './Book/book.module';
 import { BooksService } from './Book/book.service';
 import { GenresModule } from './Genre/genre.module';
-
+import { Book } from './Book/schemas/book.schema';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://emma:emma@bookscluster.jrr4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
     BooksModule,
     GenresModule,
+    MongooseModule.forFeature([
+			{ name: Book.name, schema: BookSchema },
+		]),
   ],
   controllers: [
     AppController,
